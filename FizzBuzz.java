f377a150b0119864379d0e545e70285bae101661
relyt6749 <relyt.6749@gmail.com>
2016-08-28 21:06:47 -0600

    Add files via upload

---
+++ FizzBuzz.java
@@ -0,0 +1,27 @@
+/*
+Name: Tyler Cooper
+HW#: 1
+Title: FizzBuzz
+Outside Resource: Faound at http://codereview.stackexchange.com/questions/11489/fizzbuzz-implementation
+*/
+public class FizzBuzz {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            boolean fizzOrBuzz = false;
+            if (i % 3 == 0) {
+                System.out.print("Fizz");
+                fizzOrBuzz = true;
+            }
+            if (i % 5 == 0) {
+                System.out.print("Buzz");
+                fizzOrBuzz = true;
+            }
+
+            if (fizzOrBuzz) {
+                System.out.println();
+            } else {
+                System.out.println(String.valueOf(i));
+            }
+        }
+    }
+}
