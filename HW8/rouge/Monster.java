dff11f7d21897fa13c15263d03b275c2136a6088
relyt6749 <relyt.6749@gmail.com>
2017-12-07 21:43:36 -0700

    Add files via upload

--- HW8/rouge/Monster.java
+++ HW8/rouge/Monster.java
@@ -22,18 +22,297 @@ public class Monster {
         Site monster = game.getMonsterSite();
         Site rogue   = game.getRogueSite();
         Site move    = null;
+        
+        //my code
+        Site site = null;
+        Site site2 = null;
+        Site site3 = null;
+        Site site4 = null;
+        int mony = monster.i();
+        int monx = monster.j();
+        int roguey = rogue.i();
+        int roguex = rogue.j();
+        
+        if(roguey > mony && roguex > monx)
+        {
+            site = new Site(mony+1, monx+1);
+            if (dungeon.isLegalMove(monster, site))
+            {
+                move = site;
+            }
 
+            else
+            {
+               site = new Site(mony+1,monx);
+               site2 = new Site(mony,monx+1);
+               site3 = new Site(mony,monx-1);
+               site4 = new Site(mony-1,monx);
+               if (dungeon.isLegalMove(monster, site))
+               {
+                   move = site;
+               }
+               else if(dungeon.isLegalMove(monster, site2))
+               {
+                   move = site;
+               }
+               else if(dungeon.isLegalMove(monster, site3))
+               {
+                   move = site;
+               }
+               else if(dungeon.isLegalMove(monster, site4))
+               {
+                   move = site;
+               }
+            }
+        }
+        
+        if(roguey > mony && roguex == monx)
+        {
+            site = new Site(mony+1, monx);
+            if (dungeon.isLegalMove(monster, site))
+            {
+                move = site;
+            }
+
+            else
+            {
+               site = new Site(mony+1,monx);
+               site2 = new Site(mony,monx+1);
+               site3 = new Site(mony,monx-1);
+               site4 = new Site(mony-1,monx);
+               if (dungeon.isLegalMove(monster, site))
+               {
+                   move = site;
+               }
+               else if(dungeon.isLegalMove(monster, site2))
+               {
+                   move = site;
+               }
+               else if(dungeon.isLegalMove(monster, site3))
+               {
+                   move = site;
+               }
+               else if(dungeon.isLegalMove(monster, site4))
+               {
+                   move = site;
+               }
+            }
+        }
+        
+        if(roguey > mony && roguex < monx)
+        {
+            site = new Site(mony+1, monx-1);
+            if (dungeon.isLegalMove(monster, site))
+            {
+                move = site;
+            }
+
+            else
+            {
+               site = new Site(mony+1,monx);
+               site2 = new Site(mony,monx-1);
+               site3 = new Site(mony,monx+1);
+               site4 = new Site(mony-1,monx);
+               if (dungeon.isLegalMove(monster, site))
+               {
+                   move = site;
+               }
+               else if(dungeon.isLegalMove(monster, site2))
+               {
+                   move = site;
+               }
+               else if(dungeon.isLegalMove(monster, site3))
+               {
+                   move = site;
+               }
+               else if(dungeon.isLegalMove(monster, site4))
+               {
+                   move = site;
+               }
+            }
+        }
+        
+        if(roguey < mony && roguex > monx)
+        {
+            site = new Site(mony-1, monx+1);
+            if (dungeon.isLegalMove(monster, site))
+            {
+                move = site;
+            }
+
+            else
+            {
+               site = new Site(mony-1,monx);
+               site2 = new Site(mony,monx+1);
+               site3 = new Site(mony,monx-1);
+               site4 = new Site(mony+1,monx);
+               if (dungeon.isLegalMove(monster, site))
+               {
+                   move = site;
+               }
+               else if(dungeon.isLegalMove(monster, site2))
+               {
+                   move = site;
+               }
+               else if(dungeon.isLegalMove(monster, site3))
+               {
+                   move = site;
+               }
+               else if(dungeon.isLegalMove(monster, site4))
+               {
+                   move = site;
+               }
+            }
+        }
+        
+        if(roguey < mony && roguex == monx)
+        {
+            site = new Site(mony-1, monx);
+            if (dungeon.isLegalMove(monster, site))
+            {
+                move = site;
+            }
+
+            else
+            {
+               site = new Site(mony-1,monx);
+               site2 = new Site(mony,monx+1);
+               site3 = new Site(mony,monx-1);
+               site4 = new Site(mony+1,monx);
+               if (dungeon.isLegalMove(monster, site))
+               {
+                   move = site;
+               }
+               else if(dungeon.isLegalMove(monster, site2))
+               {
+                   move = site;
+               }
+               else if(dungeon.isLegalMove(monster, site3))
+               {
+                   move = site;
+               }
+               else if(dungeon.isLegalMove(monster, site4))
+               {
+                   move = site;
+               }
+            }
+        }
+        
+        if(roguey < mony && roguex < monx)
+        {
+            site = new Site(mony-1, monx-1);
+            if (dungeon.isLegalMove(monster, site))
+            {
+                move = site;
+            }
+
+            else
+            {
+               site = new Site(mony-1,monx);
+               site2 = new Site(mony,monx-1);
+               site3 = new Site(mony,monx+1);
+               site4 = new Site(mony+1,monx);
+               if (dungeon.isLegalMove(monster, site))
+               {
+                   move = site;
+               }
+               else if(dungeon.isLegalMove(monster, site2))
+               {
+                   move = site;
+               }
+               else if(dungeon.isLegalMove(monster, site3))
+               {
+                   move = site;
+               }
+               else if(dungeon.isLegalMove(monster, site4))
+               {
+                   move = site;
+               }
+            }
+        }
+        
+        if(roguey == mony && roguex > monx)
+        {
+            site = new Site(mony, monx+1);
+            if (dungeon.isLegalMove(monster, site))
+            {
+                move = site;
+            }
+
+            else
+            {
+               site = new Site(mony,monx+1);
+               site2 = new Site(mony-1,monx);
+               site3 = new Site(mony+1,monx);
+               site4 = new Site(mony,monx-1);
+               if (dungeon.isLegalMove(monster, site))
+               {
+                   move = site;
+               }
+               else if(dungeon.isLegalMove(monster, site2))
+               {
+                   move = site;
+               }
+               else if(dungeon.isLegalMove(monster, site3))
+               {
+                   move = site;
+               }
+               else if(dungeon.isLegalMove(monster, site4))
+               {
+                   move = site;
+               }
+            }
+        }
+        
+        if(roguey == mony && roguex < monx)
+        {
+            site = new Site(mony, monx-1);
+            if (dungeon.isLegalMove(monster, site))
+            {
+                move = site;
+            }
+
+            else
+            {
+               site = new Site(mony,monx-1);
+               site2 = new Site(mony+1,monx);
+               site3 = new Site(mony-1,monx);
+               site4 = new Site(mony,monx+1);
+               if (dungeon.isLegalMove(monster, site))
+               {
+                   move = site;
+               }
+               else if(dungeon.isLegalMove(monster, site2))
+               {
+                   move = site;
+               }
+               else if(dungeon.isLegalMove(monster, site3))
+               {
+                   move = site;
+               }
+               else if(dungeon.isLegalMove(monster, site4))
+               {
+                   move = site;
+               }
+            }
+        }
+        /************************************end my code***************************************************/
+        
+        
         // take random legal move
+        /*
         int n = 0;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 Site site = new Site(i, j);
                 if (dungeon.isLegalMove(monster, site)) {
                     n++;
-                    if (Math.random() <= 1.0 / n) move = site;
+                    if (Math.random() <= 1.0 / n) 
+                        move = site;
                 }
             }
         }
+        */
         return move;
     }
 
--- HW8/rouge/Rogue.java
+++ HW8/rouge/Rogue.java
@@ -2,7 +2,10 @@
  *  Compilation:  javac Rogue.java
  * 
  *************************************************************************/
-
+/*my code*/
+import java.io.*;
+import java.util.*;
+/*end my code*/
 public class Rogue {
     private Game game;
     private Dungeon dungeon;
@@ -22,8 +25,197 @@ public class Rogue {
         Site monster = game.getMonsterSite();
         Site rogue   = game.getRogueSite();
         Site move    = null;
+        
+        /****************************my code***********************************/
+        Scanner scanner = new Scanner(System.in);
+        String choice = "";
+        boolean loop = true;
+        Site site = null;
+        int roguey = rogue.i();
+        int roguex = rogue.j();
+        
+        System.out.println("Would you like to go:" + 
+                              "\n\t8: North" + 
+                              "\n\t9: North East" + 
+                              "\n\t6: East" + 
+                              "\n\t3: South East" + 
+                              "\n\t2: South" + 
+                              "\n\t1: South West" + 
+                              "\n\t4: West" + 
+                              "\n\t7: North west" + 
+                              "\n\t5: Stay Sill");
+        
+        while(loop)
+        {
+            choice = scanner.nextLine();
+            
+            switch(choice)
+            {
+                case "8"://N
+                    
+                    site = new Site(roguey-1, roguex);
+                    if (dungeon.isLegalMove(rogue, site))
+                    {
+                        move = site;
+                        loop = false;
+                    }
+                    else
+                    {
+                        System.out.println("Can't go that way. Lose your turn!");
+                        loop = false;
+                        site = new Site(roguey, roguex);
+                        return site;
+                    }
+                    
+                    break;
+                    
+                case "9"://NE
+                    
+                    site = new Site(roguey-1, roguex+1);
+                    if (dungeon.isLegalMove(rogue, site))
+                    {
+                        move = site;
+                        loop = false;
+                    }
+                    else
+                    {
+                        System.out.println("Can't go that way. Lose your turn!");
+                        loop = false;
+                        site = new Site(roguey, roguex);
+                        return site;
+                    }
+                    
+                    break;
+                
+                case "6"://E
+                    
+                    site = new Site(roguey, roguex+1);
+                    if (dungeon.isLegalMove(rogue, site))
+                    {
+                        move = site;
+                        loop = false;
+                    }
+                    else
+                    {
+                        System.out.println("Can't go that way. Lose your turn!");
+                        loop = false;
+                        site = new Site(roguex, roguey);
+                        return site;
+                    }
+                    
+                    break;
+                    
+                case "3"://SE
+                    
+                    site = new Site(roguey+1, roguex+1);
+                    if (dungeon.isLegalMove(rogue, site))
+                    {
+                        move = site;
+                        loop = false;
+                    }
+                    else
+                    {
+                        System.out.println("Can't go that way. Lose your turn!");
+                        loop = false;
+                        site = new Site(roguey, roguex);
+                        return site;
+                    }
+                    
+                    break;
+                    
+                case "2"://S
+                    
+                    site = new Site(roguey+1, roguex);
+                    if (dungeon.isLegalMove(rogue, site))
+                    {
+                        move = site;
+                        loop = false;
+                    }
+                    else
+                    {
+                        System.out.println("Can't go that way. Lose your turn!");
+                        loop = false;
+                        site = new Site(roguey, roguex);
+                        return site;
+                    }
+                    
+                    break;
+                    
+                case "1"://SW
+                    
+                    site = new Site(roguey+1, roguex-1);
+                    if (dungeon.isLegalMove(rogue, site))
+                    {
+                        move = site;
+                        loop = false;
+                    }
+                    else
+                    {
+                        System.out.println("Can't go that way. Lose your turn!");
+                        loop = false;
+                        site = new Site(roguey, roguex);
+                        return site;
+                    }
+                    
+                    break;
+                
+                case "4"://W
+                    
+                    site = new Site(roguey, roguex-1);
+                    if (dungeon.isLegalMove(rogue, site))
+                    {
+                        move = site;
+                        loop = false;
+                    }
+                    else
+                    {
+                        System.out.println("Can't go that way. Lose your turn!");
+                        loop = false;
+                        site = new Site(roguey, roguex);
+                        return site;
+                    }
+                    
+                    break;
+                    
+                case "7"://NW
+                    
+                    site = new Site(roguey-1, roguex-1);
+                    if (dungeon.isLegalMove(rogue, site))
+                    {
+                        move = site;
+                        loop = false;
+                    }
+                    else
+                    {
+                        System.out.println("Can't go that way. Lose your turn!");
+                        loop = false;
+                        site = new Site(roguey, roguex);
+                        return site;
+                    }
+                    
+                    break;
+                
+                case "5"://Stay
+                    
+                    site = new Site(roguey, roguex);
+                    if (dungeon.isLegalMove(rogue, site))
+                    {
+                        move = site;
+                        loop = false;
+                    }
+                    
+                    break;
+                    
+                default:
+                
+                  System.out.println("InValid Selection. Try again...");
+            }
+        }
+        
+        /****************************end my code***********************************/
 
         // take random legal move
+        /*
         int n = 0;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
@@ -34,6 +226,7 @@ public class Rogue {
                 }
             }
         }
+        */
         return move;
     }
 
